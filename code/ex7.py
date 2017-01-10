fe3d72c7f5b4ea12bd008e876770ba09561c1cc4
verywindy zhou <verywindy zhou>
2017-01-10 13:42:16 +0800

    new file

---
+++ code/ex7.py
@@ -0,0 +1,22 @@
+# -*- coding:utf-8 -*-
+print "Mary had a little lamb."
+print "Its fleece was white as %s." % 'snow'
+print "And everwhere that Mary went." # 输出 “”里的内容
+print "." * 10 # what'd that do? 输出10个.
+
+end1 = "C"
+end2 = "h"
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "e"
+end7 = "B"
+end8 = "u"
+end9 = "r"
+end10 = "g"
+end11 = "e"
+end12 = "r" # 变量end12就是字符串 r 
+
+# watch that comma at the end. try removing it to see what happens
+print end1 + end2 + end3 + end4 + end5 +end6, #打印（输出）end1-end6几个字符串连起来的字符串
+print end7 + end8 + end9 + end10 + end11 + end12 # 打印（输出）end7到end12几个字符串连起来的字符串
