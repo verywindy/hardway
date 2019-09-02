df002f84ed5b2abc02c649641b8ebe9ca8ad3110
verywindy <verywindy99@hotmail.com>
2019-09-02 17:58:40 +0800

    del code

--- code/ex10.py
+++
@@ -1,4 +0,0 @@
-# -*- coding:utf-8 -*-
-while True:
-   for i in ["/","-","|","\\","|"]:
-       print "%s\r" % i,
--- code/ex12.py
+++
@@ -1,7 +0,0 @@
-# -*- coding:utf-8
-age = raw_input("How old are you?")
-height = raw_input("How tall are you?")
-weigh = raw_input("How much do you weigh?")
-
-print "So, you're %r old, %r tall and %r heavy." % (
-    age, height, weigh)
